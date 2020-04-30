b54fdbdba70ed1853708f6f5bf7552e8a18fc60e
NoahScafati <63928361+NoahScafati@users.noreply.github.com>
2020-04-30 13:14:24 -0400

    Add files via upload

---
+++ src/src/MovieRow.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class MovieRow extends React.Component{
+
+
+    render() {
+        return <table key={this.props.movie.id} className = "searchResults">
+        <tbody>
+          <tr className = "searchResultRow">
+            <td>
+              <img alt = "poster" id = "movieposter" width = "50" src = {this.props.movie.poster_src}/> 
+            </td>
+            <td className = "searchResultRow">
+              <p>{this.props.movie.title}</p>
+              <p>{this.props.movie.release_date}</p>
+              <input type = "button" value="Select" />
+            </td>
+            <td>
+              <p>{this.props.movie.overview}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    }
+}
+
+export default MovieRow
