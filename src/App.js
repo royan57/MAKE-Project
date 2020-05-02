fb9f928ef4b282000dda645d40d2d1b1c4c6d402
NoahScafati <63928361+NoahScafati@users.noreply.github.com>
2020-05-01 20:00:47 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
     this.cancel = '';
   }
- 
+    // Takes the searchterm and attatches it url for querying
     performSearch(searchTerm) {
       const urlString = "https://api.themoviedb.org/3/search/movie?api_key=0e11dba550c2721f8769af2eb1eec6b5&query=" + searchTerm
       $.ajax({
@@ -30,11 +30,13 @@ class App extends Component {
             return
           }
           
+
           results.forEach((movie) => {
             movie.poster_src = "https://image.tmdb.org/t/p/w188_and_h282_bestv2" + movie.poster_path
             console.log(movie.poster_path)
             const movieRow = <MovieRow key={movie.id} movie={movie}/>
             
+            // For each movie results, a row is created 
             movieRows.push(movieRow)
 
           })
@@ -48,6 +50,7 @@ class App extends Component {
     }
   
   searchChangeHandler(event) {
+    // checks to ensure that the page is loading correctly and searchbar is empty
     console.log("TestString "+ event.target.value)
     if (event.target.value === "") {
       this.setState({loading: false})
@@ -64,7 +67,7 @@ class App extends Component {
   }
 
 
-
+// Main page rendering containing the obama images and title
   render() {
     return (
       <div> 
--- src/MovieRow.js
+++ src/MovieRow.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 class MovieRow extends React.Component{
 
-
+// When called upon, this renders a table containing the movie poster next to its title, release Date, and overview
     render() {
         return <table key={this.props.movie.id} className = "searchResults">
         <tbody>
@@ -14,6 +14,7 @@ class MovieRow extends React.Component{
             <td className = "searchResultRow">
               <p>{this.props.movie.title}</p>
               <p>{this.props.movie.release_date}</p>
+              {/* Add onClick value to button to select image to go onto the poster position */}
               <input type = "button" value="Select"/>
             </td>
             <td>
